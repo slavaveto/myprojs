e716929a89ffe95c9464579f451b85145cdb99f0
me <slavaveto@gmail.com>
2025-12-25 16:36:34 -0300

    2345

--- app/components/TaskRow.tsx
+++ app/components/TaskRow.tsx
@@ -33,21 +33,22 @@ export const TaskRow = ({ task, onUpdate, onDelete, isOverlay }: TaskRowProps) =
          ref={setNodeRef}
          style={style}
          className={clsx(
-            'group px-2 flex justify-between h-[30px]  items-center  rounded-lg border border-default-300 bg-content1 transition-colors outline-none',
+            'group px-1 flex justify-between h-[30px]  items-center  rounded-lg border border-default-300 bg-content1 transition-colors outline-none',
             !isDragging && !isOverlay && 'hover:bg-default-50',
             (isDragging || isOverlay) &&
                'z-50 bg-default-100 border-primary/50 pointer-events-none cursor-grabbing'
          )}
       >
          {/* Drag Handle + Checkbox Wrapper */}
-         <div className="flex flex-1 gap-2 flex-row items-center justify-center">
+         <div className="flex flex-1 gap-1 flex-row items-center justify-center">
             <div
                {...attributes}
                {...listeners}
-               className="cursor-grab  active:cursor-grabbing text-default-400 hover:text-default-600 outline-none hover:bg-default-100 rounded text-center"
+               className="cursor-grab p-[2px] active:cursor-grabbing text-default-400 hover:text-default-600 outline-none hover:bg-default-100 rounded text-center"
             >
-               <GripVertical size={14} />
+               <GripVertical size={16} />
             </div>
+
 
             <Checkbox
                isSelected={task.is_completed}
@@ -55,7 +56,7 @@ export const TaskRow = ({ task, onUpdate, onDelete, isOverlay }: TaskRowProps) =
                classNames={{
                   wrapper: 'after:bg-primary',
                }}
-               className={clsx(' p-0 m-0 text-center !w-[16px]')}
+               className={clsx(' p-0 m-0 text-center  !w-[16px] mx-0')}
                size="sm"
                //   className="scale-90"
             />
@@ -64,44 +65,19 @@ export const TaskRow = ({ task, onUpdate, onDelete, isOverlay }: TaskRowProps) =
                onSave={(val) => onUpdate(task.id, { content: val })}
                isMultiline
                className={clsx(
-                  'text-[16px] font-medium w-full p-0 m-0 mr-1',
+                  'text-[16px] font-medium w-full p-0 m-0 pl-1 mr-2',
                   task.is_completed && 'text-default-400 line-through'
                )}
             />
          </div>
 
-         {/* Content */}
-         {/* <div className="p-0 text-start  relative whitespace-normal flex items-center ">
-            <div className="flex items-center gap-0 w-full">
-                <Checkbox
-                    isSelected={task.is_completed}
-                    onValueChange={(isSelected) => onUpdate(task.id, { is_completed: isSelected })}
-                    classNames={{
-                        wrapper: "after:bg-primary"
-                    }}
-                    className={clsx(
-                     " p-0 m-0",
-                  )}
-                    size="sm"
-                  //   className="scale-90"
-                />
-                <EditableCell
-                   value={task.content}
-                   onSave={(val) => onUpdate(task.id, { content: val })}
-                   isMultiline
-                   className={clsx(
-                      "text-[16px] font-medium w-full p-0 m-0",
-                      task.is_completed && "text-default-400 line-through"
-                   )}
-                />
-            </div>
-         </div> */}
+     
 
          {/* Actions */}
          <div className="p-0 text-center relative flex justify-center">
             <button
                onClick={() => onDelete(task.id)}
-               className="opacity-0 group-hover:opacity-100  text-default-400 hover:text-danger hover:bg-danger/10 rounded transition-all"
+               className="opacity-0 p-[2px] group-hover:opacity-100  text-default-400 hover:text-danger hover:bg-danger/10 rounded transition-all"
                aria-label="Delete task"
             >
                <Trash2 size={16} />
